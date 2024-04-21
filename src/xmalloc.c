978075f48c5976a4b9c4bb5b462557e8b987bc2c
Cyuria <55673467+Cyuria@users.noreply.github.com>
2024-04-22 02:42:51 +1000

    nvm about mac/homebrew I forgot a '

--- src/xmalloc.c
+++ src/xmalloc.c
@@ -9,7 +9,7 @@ void *xmalloc(size_t sz) {
   void *ptr = malloc(sz);
   if (!ptr) {
     logger(CRITICAL, error_system, "Call to stdlib function malloc failed");
-    exit(EXIT_FAILURE);
+    abort();
   }
   return ptr;
 }
