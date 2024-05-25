e8b3817665f82cd7bf378bde277f8f1a741d9190
Cyuria <55673467+cyuria@users.noreply.github.com>
2024-05-25 17:30:41 +1000

    feat: Add rv64a source files

---
+++ h/form/rv64a.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "form/instructions.h"
+
+extern const struct formation_t rv64a[];
---
+++ src/form/rv64a.c
@@ -0,0 +1,10 @@
+
+#include "form/rv64a.h"
+
+#include "form/generic.h"
+#include "parse.h"
+
+const struct formation_t rv64a[] = {
+
+	END_FORMATION
+};
